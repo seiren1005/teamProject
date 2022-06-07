8d404787bbcb22caf86786dd4d0a1e09f5e4a4cc
hyewon1004 <jamir508@naver.com>
2022-06-07 12:55:12 +0900

    commit

--- it5th/src/main/java/com/globalin/teamproject/HomeController.java
+++ it5th/src/main/java/com/globalin/teamproject/HomeController.java
@@ -25,7 +25,7 @@ public class HomeController {
 			// model : 데이터를 담는 그릇 역할, map구조로 저장된다.
 			// model.addAttribute("변수명", 값);
 			model.addAttribute("msg", "홈페이지 방문을 환영합니다!");
-			return "main"; // main.jsp로 포워딩
+			return "home"; // main.jsp로 포워딩
 		}
 		
 		// url mapping
