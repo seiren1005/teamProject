8da3175f7e0b7885c1756e6a90353d192ff5bd2d
hyewon1004 <jamir508@naver.com>
2022-06-07 10:32:35 +0900

    commit

--- it5th/src/main/java/com/globalin/model/dao/MemberDAOImpl.java
+++ it5th/src/main/java/com/globalin/model/dao/MemberDAOImpl.java
@@ -16,32 +16,32 @@ import com.globalin.model.dto.MemberVO;
 public class MemberDAOImpl implements MemberDAO {
 	
 	@Inject
-	SqlSession SqlSession;
+	SqlSession sqlSession;
 
 	@Override
 	public List<MemberVO> memberList() {
-		return SqlSession.selectList("member.memberList");
+		return sqlSession.selectList("member.memberList");
 	}
 	
 	@Override
 	public void insertMember(MemberVO vo) {
-		SqlSession.insert("member.insertMember", vo);
+		sqlSession.insert("member.insertMember", vo);
 		
 	}
 
 	@Override
 	public MemberVO viewMember(String userId) {
-		return SqlSession.selectOne("member.viewMember", userId);
+		return sqlSession.selectOne("member.viewMember", userId);
 	}
 
 	@Override
 	public void deleteMember(String userId) {
-		SqlSession.delete("member.deleteMember",userId);
+		sqlSession.delete("member.deleteMember",userId);
 	}
 
 	@Override
 	public void updateMember(MemberVO vo) {
-		SqlSession.update("member.updateMember", vo);
+		sqlSession.update("member.updateMember", vo);
 		
 	}
 
@@ -51,20 +51,20 @@ public class MemberDAOImpl implements MemberDAO {
 		Map<String, String> map = new HashMap<String, String>();
 		map.put("userId", userId);
 		map.put("userPw", userPw);
-		int count = SqlSession.selectOne("member.checkPw", map);
+		int count = sqlSession.selectOne("member.checkPw", map);
 		if(count == 1) result= true;
 		return result;
 	}
 	// 01_01. 회원 로그인체크
 		@Override
 		public boolean loginCheck(MemberVO vo) {
-			String name = SqlSession.selectOne("member.loginCheck", vo);
+			String name = sqlSession.selectOne("member.loginCheck", vo);
 			return (name == null) ? false : true;
 		}
 		// 01_02. 회원 로그인 정보
 		@Override
 		public MemberVO viewMember(MemberVO vo) {
-			return SqlSession.selectOne("member.viewMember", vo);
+			return sqlSession.selectOne("member.viewMember", vo);
 		}
 		// 02. 회원 로그아웃
 		@Override
